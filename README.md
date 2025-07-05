a52c985f5402f32880742bbc450e2e0604de98ec
Jeff Long <banterability@users.noreply.github.com>
2025-07-05 15:02:20 -0500

    Prettier

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-mainspring
-----------
+## mainspring
 
 [![Latest Version](https://img.shields.io/npm/v/mainspring.svg)](https://www.npmjs.com/package/mainspring)
 [![🚦 CI](https://github.com/banterability/mainspring/actions/workflows/ci.yml/badge.svg)](https://github.com/banterability/mainspring/actions/workflows/ci.yml)
@@ -13,7 +12,7 @@ npm install mainspring
 ### Usage
 
 ```javascript
-const mainspring = require('mainspring');
+const mainspring = require("mainspring");
 
 // Compare a date against now
 mainspring(new Date(2023, 8, 24));
@@ -26,10 +25,7 @@ mainspring(new Date(2023, 8, 24));
 } */
 
 // Compare two dates
-mainspring(
-  new Date(1962, 8, 12),
-  new Date(1969, 5, 20)
-);
+mainspring(new Date(1962, 8, 12), new Date(1969, 5, 20));
 /* -> {
   inFuture: false,
   days: 2473,
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-const mainspring = require('../');
-const timekeeper = require('timekeeper');
+const mainspring = require("./index.js");
+const timekeeper = require("timekeeper");
 
 const buildMainspringResponse = (attributes) => {
   const output = {};
@@ -9,76 +9,76 @@ const buildMainspringResponse = (attributes) => {
   output.seconds = attributes.seconds || 0;
   output.inFuture = false;
   return output;
-}
+};
 
-describe('Mainspring', function() {
-  describe('comparing two dates', function() {
-    it('15 seconds ago', function() {
+describe("Mainspring", function () {
+  describe("comparing two dates", function () {
+    it("15 seconds ago", function () {
       var d1 = new Date(2015, 3, 1, 14, 3, 45);
       var d2 = new Date(2015, 3, 1, 14, 4, 0);
-      var expected = buildMainspringResponse({seconds: 15});
+      var expected = buildMainspringResponse({ seconds: 15 });
 
       expect(mainspring(d1, d2)).toEqual(expected);
     });
 
-    it('15 minutes ago', function() {
+    it("15 minutes ago", function () {
       var d1 = new Date(2015, 3, 1, 13, 49, 0);
       var d2 = new Date(2015, 3, 1, 14, 4, 0);
-      var expected = buildMainspringResponse({minutes: 15});
+      var expected = buildMainspringResponse({ minutes: 15 });
 
       expect(mainspring(d1, d2)).toEqual(expected);
     });
 
-    it('15 hours ago', function() {
+    it("15 hours ago", function () {
       var d1 = new Date(2015, 2, 31, 23, 4, 0);
       var d2 = new Date(2015, 3, 1, 14, 4, 0);
-      var expected = buildMainspringResponse({hours: 15});
+      var expected = buildMainspringResponse({ hours: 15 });
 
       expect(mainspring(d1, d2)).toEqual(expected);
     });
 
-    it('15 days ago', function() {
+    it("15 days ago", function () {
       var d1 = new Date(2015, 2, 17, 14, 4, 0);
       var d2 = new Date(2015, 3, 1, 14, 4, 0);
-      var expected = buildMainspringResponse({days: 15});
+      var expected = buildMainspringResponse({ days: 15 });
 
       expect(mainspring(d1, d2)).toEqual(expected);
     });
   });
 
-  describe('comparing to now dates', function() {
-    beforeAll(function(){
+  describe("comparing to now dates", function () {
+    beforeAll(function () {
       timekeeper.freeze(new Date(2015, 3, 1, 14, 4, 0));
     });
 
-    afterAll(function(){
+    afterAll(function () {
       timekeeper.reset();
     });
 
-    it('15 seconds ago', function() {
+    it("15 seconds ago", function () {
       var d1 = new Date(2015, 3, 1, 14, 3, 45);
-      var expected = buildMainspringResponse({seconds: 15});
+      var expected = buildMainspringResponse({ seconds: 15 });
 
       expect(mainspring(d1)).toEqual(expected);
     });
 
-    it('15 minutes ago', function() {
+    it("15 minutes ago", function () {
       var d1 = new Date(2015, 3, 1, 13, 49, 0);
-      var expected = buildMainspringResponse({minutes: 15});
+      var expected = buildMainspringResponse({ minutes: 15 });
 
       expect(mainspring(d1)).toEqual(expected);
     });
 
-    it('15 hours ago', function() {
+    it("15 hours ago", function () {
       var d1 = new Date(2015, 2, 31, 23, 4, 0);
-      var expected = buildMainspringResponse({hours: 15})
+      var expected = buildMainspringResponse({ hours: 15 });
 
       expect(mainspring(d1)).toEqual(expected);
     });
 
-    it('15 days ago', function() {
+    it("15 days ago", function () {
       var d1 = new Date(2015, 2, 17, 14, 4, 0);
-      var expected = buildMainspringResponse({days: 15});
+      var expected = buildMainspringResponse({ days: 15 });
 
       expect(mainspring(d1)).toEqual(expected);
     });
